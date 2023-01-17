bb69bf3021c1f84f26f8018a926e3e74623c1017
jesussoga <em.sotoca@gmail.com>
2023-01-17 16:01:30 +0100

    Empezamos con este proyecto de nuevo.

--- js/miCodigo.js
+++ js/miCodigo.js
@@ -1,98 +1,108 @@
-/*
-var nombres = ["loli","enrique","diego","jesus","Paco","javi", "gavi"];
-var nombreAleatorio = nombres.length-1;
-var numeroAleatorio = Math.floor(Math.random() * (nombreAleatorio));
-var numeroAleatorio1 = Math.floor(Math.random() * (nombreAleatorio));
-var nombreSelcA = nombres[numeroAleatorio];
-var nombreSelcB= nombres[numeroAleatorio1];
-if (nombreSelcA != nombreSelcB) {
-    var res1 = nombreSelcA +" "+nombreSelcB    
-}else{
-    var res1 = nombreSelcA +" "+nombreSelcB    
-    
-}
-console.log(res1);
-*/
+var lista = []; //creamos un array fuera de la llamada para guardar los cambios
+var listaAmigos = []
+nombre.focus(); //ponemos el foco en el input
+const btnIntroducir = document.getElementById("btnIntroducir");
+btnIntroducir.disabled = true // desactivamos el boton
+btnIntroducir.addEventListener("click", function () {
+   // escuchamos el boton y realizamos funcion para es evento
+   var nombre = document.querySelector("#nombre");//hacemos referencia al input
+   nombre.focus(); //ponemos el foco en el input
+   
+   if (nombre.value === "" && nombre.value === " ") {
+      btnIntroducir.disabled =false
+   }else if (nombre.value != "" && nombre.value != " ") {
+      btnIntroducir.disabled =true
+      
+   } else {
+      btnIntroducir.disabled =true
+      
+   }
+   lista.push(nombre.value);
+   
+   //generamos lista ordenada de los nombres
+   var contenido = nombre.value;
+   var li1 = document.createElement("li");   
+   var p1 = document.createElement("p");
+   p1.appendChild(document.createTextNode(contenido));
+   document.querySelector("#lista").appendChild(li1).appendChild(p1);  
+   nombre.value = " "
+})
+   
 
+//funcion para activar el evento producido en el input 
+function activar() {
+   btnIntroducir.disabled = false
+}  
+
+  //hacemos referencia al boton de Parejas y creamos un evento al escuchar el boton   
+  var arrayResultado = []
+  var arrayNumeros = []
+  var listaB = [] 
+  var listaC = []
+const btnParejas = document.getElementById("btnParejas");
+btnParejas.addEventListener("click", function () {
+   //generamos numeros aleatorios sin que se repitan y los metemos en un array
+   
+   console.log(lista);
+   //hacemos recorrer el array de numeros atraves del array de nombres creando un array de nombres desordenados
+   for (let index = 0; index < lista.length ; index++) {  //no es lista.lenght -1
+      listaB.push(lista[index])
+   }
+   listaB.sort(function() {return Math.random() - 0.5});
+   console.log(listaB);
+   for (let index = 0; index < lista.length ; index++) {  //no es lista.lenght -1
+      listaC.push(listaB[index])
+   }
+   listaC.push(listaB[0]);
+   listaC.shift()
+   
+   console.log(listaC);
+       //generamos lista desordenada 
+   for (let index = 0; index < lista.length ; index++) {
+      
+      var li = document.createElement("li");
+      var p = document.createElement("p");
+      var contenido2 = listaB[index]+ " regala a " + listaC[index];
+      p.appendChild(document.createTextNode(contenido2));
+      document.querySelector("#listaNombres").appendChild(li).appendChild(p);        
+      arrayResultado.push(contenido2);
+      
+   }
+   btnParejas.disabled = true;
+   
+   
+   
+   
+   
+})
+const btnReinicio = document.getElementById("btnReinicio");
+btnReinicio.addEventListener("click",function() {
+   location.reload();
+} )
 /*
-/* Muestra un número aleatorio entre max y min y lo mete en un elemento con id dado
-/* Sino me pasan variables tomo por defecto 1 y 6
-*/
-function indiceAleatorio(max, min) { 
-    var aleatorio = Math.floor((Math.random() * (max - min + 1)) + min);
-    return aleatorio;
- }
- 
- /**
-  * Devuelve el array de elementos contenidos en el objeto identificado
-  * @param {*} identificador 
-  * @returns 
-  */
- function selectToArray (identificador) {
-    var selector = "#" + identificador;  //construyo el selector
-    var objeto = document.querySelector(selector);
-    return objeto.options;
- }
- 
- /**
-  * Recibe un array y devuelve un elemento aleatorio del mismo usando la funcion indiceAleatorio
-  * @param {*} array 
-  * @returns 
-  */
- function obtenerTextoAleatorio(array) {
-    var indice = indiceAleatorio(array.length - 1, 1);
-    return array[indice];
- }
- /**
-  * OPCIONAL para cargar imagen aleatoria 
-  * LA IMAGEN CAMBIA PERO TIENES QUE ESPERAR TIEMPO...
-  */
-  function imagenAleatoria(){
-    var imagen = document.querySelector("#imgPelicula");
-    //tomo la id de la foto de 1 a 1000 para coger el rango de fotos existentes en la web
-    var url = "https://picsum.photos/id/" + indiceAleatorio(1000, 1) + "/300/200";
-    imagen.removeAttribute("src");
-    imagen.src = url;
- }
- 
- 
- /* CÓDIGO PRINCIPAL */
- function generarTitulo() {
-    var selectPrimeraParte = document.querySelector("#selPrimeraParte");
-    var selectSegundaParte = document.querySelector("#selSegundaParte");
-    var selectTerceraParte = document.querySelector("#selTerceraParte");
-    var titulo = document.querySelector("#titulo");
- 
-    //actualiza la imagen
-    imagenAleatoria();
- 
-    //primer select
-    var primeraParte = selectToArray("selPrimeraParte"); //traigo el array
-    var objeto1 = obtenerTextoAleatorio(primeraParte); //guardo el objeto completo
-    var texto1 = objeto1.innerHTML; //recojo el texto
-    selectPrimeraParte.selectedIndex = objeto1.value; //marco su opción en el select
- 
- 
-    //decido si uso o no el segundo select
-    var siNo = indiceAleatorio(1, 0);
-    if(siNo == 1) {
-       var segundaParte = selectToArray("selSegundaParte"); //traigo el array
-       var objeto2 = obtenerTextoAleatorio(segundaParte); //guardo el objeto completo
-       var texto2 = " " + objeto2.innerHTML + " ";
-       selectSegundaParte.selectedIndex = objeto2.value;
-       selectSegundaParte.removeAttribute("disabled"); //si lo uso lo habilito
-    } else {
-       var texto2 = " ";
-       selectSegundaParte.selectedIndex = "0";
-       selectSegundaParte.setAttribute("disabled", "disabled"); //sino lo uso lo deshabilito
-    }
-    
-    //tercer select
-    var terceraParte = selectToArray("selTerceraParte"); //traigo el array
-    var objeto3 = obtenerTextoAleatorio(terceraParte); //guardo el objeto completo
-    var texto3 = objeto3.innerHTML;
-    selectTerceraParte.selectedIndex = objeto3.value;
- 
-    //relleno el título
-    titulo.innerHTML = texto1 +  texto2 + texto3;
- } 
+var ls = window.localStorage;
+var array = []
+//Hacemos referencia al boton guardar y escuchamos el evento
+const btnGuardar = document.getElementById("btnGuardar");
+btnGuardar.addEventListener("dblclick", function(){
+   var nombreClave = document.querySelector("#nombreClave");
+   array = lista.concat(arrayResultado);
+   console.log(array);
+   ls.setItem(nombreClave.value, array)
+})
+
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function(){
+   var nombreClave = document.querySelector("#nombreClave");
+   listaGuardada = ls.getItem(nombreClave.value);
+   console.log(listaGuardada.split(","));
+})
+console.log(Object.keys(ls).length); 
+for (let index = 0; index < Object.keys(ls).length; index++) {
+   
+   var li2 = document.createElement("li");
+   var p2 = document.createElement("p");
+   var contenido3 = Object.keys(ls)[index]
+   p2.appendChild(document.createTextNode(contenido3));
+   document.querySelector("#lista2").appendChild(li2).appendChild(p2);        
+}*/
